106c5998159a2eee07d73e931ae89c24bb318f11
amee886 <nsecex@gmail.com>
2026-04-08 22:29:32 +0300

    добавил счетчик активных пропусков

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
+    posts = Passcard.objects.all()
+    some_post=posts[0]
+    published_posts=Passcard.objects.filter(is_active=True)
+    print("Количество активных пропусков:",len(published_posts))
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
