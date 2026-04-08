ae2cc7e806cf5cbfea14848b8a8a1469e99438a8
amee886 <nsecex@gmail.com>
2026-04-08 22:28:02 +0300

    Убрал непубличные данные

--- settings.py
+++ settings.py
@@ -1,17 +1,22 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 TIME_ZONE = 'Europe/Moscow'
 
